fbe4ef4f3fd0071785cb05d147bae35d50f342b2
Zakaria Kerkeb <hikkary@MacBook-Pro-de-Zakaria.local>
2021-02-25 10:50:35 +0100

    suite

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from 'react'
-import './App.css'
-
-import CharactersList from './components/charactersList'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className='App'>
-      <CharactersList />
+    <div>
+      <Square backgroundColor='blue'></Square>
+      <Square backgroundColor='green'></Square>
+      <Square backgroundColor='yellow'></Square>
+      <Circle secondary></Circle>
+      <Circle></Circle>
+      <SquareBis border></SquareBis>
+      <SquareBis></SquareBis>
     </div>
   )
 }
+
+const SquareBis = styled.div`
+  width: 30px;
+  height: 30px;
+  border: ${props => (props.border ? props.theme.border : 'transparent')};
+`
+
+const Square = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: ${props => props.backgroundColor};
+`
+
+const backgroundColorSecondary = ({ secondary }) => {
+  if (secondary) return ''
+  else return 'red'
+}
+
+const borderSecondary = ({ secondary }) => {
+  if (secondary) return 'red'
+  else return 'transparent'
+}
+
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: ${props => backgroundColorSecondary(props)};
+  border: 1px solid ${props => borderSecondary(props)};
+`
 
 export default App
---
+++ src/config/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  primary: 'red',
+  secondary: 'transparent',
+  borderSize: '1px',
+  borderConsistance: 'Solid',
+  borderColor: 'green',
+  border: '1px solid green'
+}
+
+export { theme }
